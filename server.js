a718964178552465abc87353ae42062bf2b37263
Maciej Pie <maciejpiech18@gmail.com>
2021-02-16 20:15:34 -0600

    second commit

--- server.js
+++ server.js
@@ -31,16 +31,13 @@ const Member = conn.define('member', {
     allowNull: false,
     unique: true,
   },
-
-  sponsorId: {
-    type: UUID,
-  },
 });
 
 const Booking = conn.define('booking', {
   id: {
     type: INTEGER,
     primaryKey: true,
+    allowNull: false,
   },
   startTime: {
     allowNull: false,
@@ -50,19 +47,51 @@ const Booking = conn.define('booking', {
     allowNull: false,
     type: DATE,
   },
-  bookedById: {
-    type: UUID,
-  },
-  facilityId: {
-    type: UUID,
-  },
 });
 
 Member.belongsTo(Member, { as: 'sponsor' });
 Member.hasMany(Member, { foreignKey: 'sponsorId' });
+Booking.belongsTo(Member, { as: 'bookedBy' });
+Booking.belongsTo(Facility, { as: 'facility' });
+Member.hasMany(Booking, { foreignKey: 'bookedById' });
+Facility.hasMany(Booking, { foreignKey: 'facilityId' });
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const [tennis, pool, restaurant, kacper, kevin, moe] = await Promise.all([
+    Facility.create({ fac_name: 'tennis' }),
+    Facility.create({ fac_name: 'pool' }),
+    Facility.create({ fac_name: 'restaurant' }),
+    Member.create({ first_name: 'kacper' }),
+    Member.create({ first_name: 'kevin' }),
+    Member.create({ first_name: 'moe' }),
+  ]);
+
+  const [one, two, three] = await Promise.all([
+    Booking.create({
+      id: 1,
+      startTime: '2021-02-10 12:00:00',
+      endTime: '2021-02-10 12:15:00',
+    }),
+    Booking.create({
+      id: 2,
+      startTime: '2021-02-10 1:00:00',
+      endTime: '2021-02-10 1:15:00',
+    }),
+    Booking.create({
+      id: 3,
+      startTime: '2021-02-10 2:00:00',
+      endTime: '2021-02-10 2:15:00',
+    }),
+  ]);
+
+  moe.sponsorId = kevin.id;
+  await moe.save();
+};
 
 const init = async () => {
   try {
+    await syncAndSeed();
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
   } catch (error) {
@@ -71,3 +100,5 @@ const init = async () => {
 };
 
 init();
+
+// '2016-08-09 04:05:02'
